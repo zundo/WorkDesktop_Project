0ff5c010924e57e57430a2177cb6a997077d046a
Akhtaar <khalid88.ayachia@gmail.com>
2020-07-23 14:43:41 +0200

    .

--- Workdesktop-web/src/app/app.module.ts
+++ Workdesktop-web/src/app/app.module.ts
@@ -8,13 +8,13 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { ProfileComponent } from './modules/profile/profile.component';
-import { CalendarModule, DateAdapter } from 'angular-calendar';
-import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { CalendrierComponent } from './modules/calendrier/calendrier.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProfileComponent,
+    CalendrierComponent,
   ],
   imports: [
     BrowserModule,
@@ -22,10 +22,6 @@ import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
     BrowserAnimationsModule,
     MatButtonModule,
     DefaultModule,
-    CalendarModule.forRoot({
-      provide: DateAdapter,
-      useFactory: adapterFactory,
-    }),
 
   ],
   providers: [],
